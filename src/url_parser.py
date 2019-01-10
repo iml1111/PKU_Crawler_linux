5641155c1e4f85a2adefdc511febd6a976b3a432
iml1111 <shin10256@gmail.com>
2019-01-10 16:45:00 +0900

    bug

--- src/url_parser.py
+++ src/url_parser.py
@@ -1,26 +1,33 @@
 import requests
 import time
 
+header = {
+			"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)\
+			AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
+			"Accept":"text/html,application/xhtml+xml,application/xml;\
+			q=0.9,imgwebp,*/*;q=0.8"}
+
 def URLparser(URL):
+	
 	try:
-		html = requests.get(URL, verify = False).text
+		html = requests.get(URL, verify = False, headers = header).text
 	except:
 		time.sleep(3)
 		print("Connection Error")
 		try:
-			html = requests.get(URL, verify = False).text
+			html = requests.get(URL, verify = False,  headers = header).text
 		except:
 			return None
 	return html
 
 def URLparser_con(URL, enc):
 	try:
-		html = requests.get(URL, verify = False).content.decode(enc)
+		html = requests.get(URL, verify = False,  headers = header).content.decode(enc)
 	except:
 		time.sleep(3)
 		print("Connection Error")
 		try:
-			html = requests.get(URL, verify = False).content.decode(enc)
+			html = requests.get(URL, verify = False,  headers = header).content.decode(enc)
 		except:
 			return None
 	return html	
